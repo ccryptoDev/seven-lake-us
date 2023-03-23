d2d748d5831ff917f9abd1afd4aa515b2456fb6d
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-03-23 15:49:48 -0400

    notify leads

--- API/jobs/notifyLeads.js
+++ API/jobs/notifyLeads.js
@@ -1,7 +1,9 @@
 const CronJob = require("node-cron");
 const { notifyAgentTemplate } = require("../utils/email/templates/notify-agent/notifyAgentTemplate");
 const { User } = require('../models/userModel');
+const { Op } = require("sequelize");
 const { sendTemplate } = require("../utils/email/sendTemplate");
+const { MAX_INVITES } = require("../utils/constant/maxInvites");
 
 exports.initLeadsJob = () => {
 	let schedule = "* * * * *"
@@ -13,28 +15,53 @@ exports.initLeadsJob = () => {
 		schedule = "0 */12 * * *"
 	}
 
+	schedule = "* * * * *"
+
+	const sendEmail = async ({ email, firstName, inviteUrl }) => {
+		try {
+			sendTemplate({
+				to: email,
+				subject: 'Dont forget to sign up',
+			}, await notifyAgentTemplate({ name: firstName, inviteUrl }))
+		} catch (e) {
+			console.log("Failed to notify pedning user:", e)
+		}
+	}
+
+
 
 	const scheduledJobFunction = CronJob.schedule(schedule, async () => {
 		const users = await User.findAll({
-			where: { status: 'pending' }
+			where: { status: 'pending', invitesSent: { [Op.lt]: MAX_INVITES } }
 		})
 
 		for (let i = 0; i < users.length; i++) {
-			const { email, firstName, lastName,
+			const { email, firstName, lastName, invitesSent, lastInviteDate,
 				mobilePhone: phoneNo, Sponsor: sponsor } = users[i]
+
+			const currentDate = new Date()
+			let dateTreshold = new Date(lastInviteDate || new Date())
+
 			const params = new URLSearchParams({
 				email, firstName, lastName,
 				phoneNo, sponsor
 			})
 			const inviteUrl = `${process.env.FRONTEND_DEV_URL}/#/register?` + params
-			try {
-				sendTemplate({
-					to: email,
-					subject: 'Dont forget to sign up',
-				}, await notifyAgentTemplate({ name: firstName, inviteUrl }))
-			} catch (e) {
-				console.log("Failed to notify pedning user:", e)
+
+			if (invitesSent === 1) {
+				// 3 days after signing / 3 days after first invite
+				dateTreshold.setDate(dateTreshold.getDate() + 3)
+				if (dateTreshold < currentDate) {
+					await sendEmail({ email, firstName, inviteUrl })
+				}
+			} else if (invitesSent === 2) {
+				// 10 days after signing / 7 days after second invite
+				dateTreshold.setDate(dateTreshold.getDate() + 7)
+				if (dateTreshold < currentDate) {
+					await sendEmail({ email, firstName, inviteUrl })
+				}
 			}
+			await users[i].update({ invitesSent: invitesSent + 1, lastInviteDate: new Date() })
 		}
 	});
 
