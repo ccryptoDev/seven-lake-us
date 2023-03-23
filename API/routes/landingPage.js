dde3ecc279506de014079e567afd90778b84e041
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-03-23 15:50:33 -0400

    modified landingpage

--- API/routes/landingPage.js
+++ API/routes/landingPage.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { addLandingPage, getLandingPages, updateLandingPage } = require('../controllers/landingpage');
+const { addLandingPage, getLandingPages, updateLandingPage, selectPage } = require('../controllers/landingpage');
 const { updateLandingTemplate, gettingPerticullarLandingPage, addLandingTemplate, getLandingTemplates } = require('../controllers/landingTemplate');
 
 router.get('/getPerticularLandigPage', gettingPerticullarLandingPage);
@@ -9,6 +9,7 @@ router.put('/landing/template/:id', updateLandingTemplate)
 router.post('/landing/template', addLandingTemplate)
 
 router.get('/landing/page', getLandingPages)
+router.post('/landing/page/select', selectPage)
 router.put('/landing/page/:id', updateLandingPage)
 router.post('/landing/page', addLandingPage)
 
